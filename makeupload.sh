8c2399c078b5bd7a814ed9ac13136b6fbea9b015
einarzb <einarzb@gmail.com>
2019-07-22 12:13:05 +0300

    refactor for user absolute path

--- makeupload.sh
+++ makeupload.sh
@@ -1,5 +1,7 @@
 # make c program
-docker run --rm -v /Users/zivtaller/Documents/current-projects/aodf_app:/data -w /data ev3cc make  && 
+# fetch user path 
+myPath=`pwd`;
+docker run --rm -v $myPath:/data -w /data ev3cc make  && 
 #chmod to spec
 chmod +X ./run_root_settings && 
 chmod 4755 ./run_root_settings && 
