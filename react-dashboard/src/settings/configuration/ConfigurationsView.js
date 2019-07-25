cfac051cdd568c604b9be195580f70f04777d314
einarzb <einarzb@gmail.com>
2019-07-25 13:16:10 +0300

    redux inital setup pages

---
+++ react-dashboard/src/settings/configuration/ConfigurationsView.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+
+export default class ConfigurationsView extends React.Component{
+    
+    render(){
+    
+        return (
+          <div>
+       
+          </div>
+          )
+    }
+}
