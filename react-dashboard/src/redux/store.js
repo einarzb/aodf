fb3c9484a809ffc789b1bfe52f44f00deaa28052
einarzb <einarzb@gmail.com>
2019-08-13 23:27:36 +0300

    added redux logger to index

--- react-dashboard/src/redux/store.js
+++ react-dashboard/src/redux/store.js
@@ -1,4 +1,9 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./reducers/reducersIndex";
+import logger from 'redux-logger';
 
-export default createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export default createStore (
+  rootReducer, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger)
+);
