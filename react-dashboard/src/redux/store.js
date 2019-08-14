fd8010254134664c4516b1456594d7a329798c80
einarzb <einarzb@gmail.com>
2019-08-14 13:10:48 +0300

    added redux thunk to the project

--- react-dashboard/src/redux/store.js
+++ react-dashboard/src/redux/store.js
@@ -1,9 +1,10 @@
-import { applyMiddleware, createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers/reducersIndex";
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 export default createStore (
   rootReducer, 
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(logger)
+  applyMiddleware(logger, thunk)
 );
