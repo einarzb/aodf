5d5d9d30816a33b2ee49c9af47e71eadf67a01b6
einarzb <einarzb@gmail.com>
2019-09-02 21:15:00 +0300

    trying to fix refresh bug of react router

--- react-dashboard/src/index.js
+++ react-dashboard/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom'
+
 //redux
 import { Provider } from 'react-redux';
 import store from "./redux/store";
@@ -10,12 +12,13 @@ import * as serviceWorker from './serviceWorker';
 const rootElement = document.getElementById('root');
  
 ReactDOM.render(
- <Provider store={store}>
-   <App />
- </Provider>,
- rootElement
+    <Provider store={store}>
+        <HashRouter>
+          <App />
+        </HashRouter>
+    </Provider>, rootElement
 );
- 
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
